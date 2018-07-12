f2437936e0a221e37d34ce80779db832f6fc4d32
Anna Zhukova <zhutchok@gmail.com>
2018-07-12 17:52:10 +0200

    removing empty model choice

--- pastmlapp/forms.py
+++ pastmlapp/forms.py
@@ -124,6 +124,7 @@ class AnalysisForm(ModelForm):
         m = cleaned_data.get('prediction_method', None)
         if not ('marginal' in m or 'joint' == m):
             self.fields['model'].required = False
+        return cleaned_data
 
     def save(self, commit=True):
         self.cleaned_data['id_column'] = next(i for (i, (c, _)) in enumerate(self.column_choices)
