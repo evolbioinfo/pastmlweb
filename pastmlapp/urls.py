4178feef6fd4bf7bb0e3e9bffac207697a453e57
Anna Zhukova <zhutchok@gmail.com>
2018-07-12 11:00:13 +0200

    random ids

--- pastmlapp/urls.py
+++ pastmlapp/urls.py
@@ -7,8 +7,8 @@ app_name = "pastmlapp"
 urlpatterns = [
     url(r'^run$', views.pastml, name='pastml'),
     url(r'^$', views.index, name='index'),
-    url(r'^run/(?P<id>\d+)$', views.analysis, name='analysis'),
-    url(r'^result/(?P<id>\d+)$', views.result, name='result'),
+    url(r'^run/(?P<id>[\d\w-]+)$', views.analysis, name='analysis'),
+    url(r'^result/(?P<id>[\d\w-]+)$', views.result, name='result'),
     url(r'^view/(?P<id>[\d\w-]+)/$', views.detail, name='detail'),
     url(r'^feedback$', views.feedback, name='feedback'),
 ]
